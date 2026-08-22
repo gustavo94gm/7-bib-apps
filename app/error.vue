918ed230b6d3addcf8c7809546d76737034cf9fb
Gustavo Mueller <gustavomueller94@gmail.com>
2026-08-22 14:03:10 -0300

    criar página de erro 404

---
+++ app/error.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps<{ error: NuxtError }>()
+</script>
+
+<template>
+  <UError
+    icon="i-lucide-file-x"
+    :clear="{ label: 'Voltar para a página inicial' }"
+    :error="{
+      statusCode: 404,
+      statusMessage: 'Página não encontrada.',
+      message: 'A página que você está procurando não existe.'
+    }"
+  />
+</template>
+
